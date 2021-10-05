213749ef6a133f3b9eb0ae12d1608f3f1b40f049
lily-law <lilylaw.dev@gmail.com>
2021-10-05 18:07:19 +0100

    Continue work on move method [WIP]

--- client/src/components/ChessBoard.vue
+++ client/src/components/ChessBoard.vue
@@ -99,12 +99,12 @@ export default {
         }
     },
     methods: {
-        move: function (value, toPlay) {
-            if (!value[0]) {
+        move: function (notation, toPlay) {
+            if (!notation[0]) {
                 console.warn('Empty move!') // TODO work out if this edge case needs handling
                 return false
             }
-            else if (value.length < 2) {
+            else if (notation.length < 2) {
                 console.warn('Invalid move notation with length < 2!') 
                 return false
             }
@@ -119,110 +119,155 @@ export default {
             // extract annotations
             
             // check if castling
-            if (this.value.includes('0-0-0')) {
+            if (this.notation.includes('0-0-0')) {
                 // TODO
                 // castle queenside
             }
-            else if (this.value.includes('0-0')) {
+            else if (this.notation.includes('0-0')) {
                 // TODO
                 // castle kingside
             }
 
-            const movingPeice = value[0] === value[0].toLowerCase() ? 'P' : this.value[0]
-            let takenPeice = this.value.includes('x') && this.value[this.value.indexOf('x') + 1]
-            if (takenPeice[0] && takenPeice[0] === takenPeice[0].toPlay) {
-                takenPeice = 'P'
+            const unitToMove = notation[0] === notation[0].toLowerCase() ? 'P' : this.notation[0]
+            let unitToTake = this.notation.includes('x') && this.notation[this.notation.indexOf('x') + 1]
+            if (unitToTake[0] && unitToTake[0] === unitToTake[0].toPlay) {
+                unitToTake = 'P'
             }
-            const move = this.move.substr(this.move.length-2)
-            const specifiedOrig = this.move[this.move.length-3].match(/[a-g1-8]/)?.[0] || this.move[this.move.length-4].match(/[a-g1-8]/)?.[0]
+            const move = notation.substr(notation.length-2).match(/[a-g]\d/)?.[0]
+            const specifiedOrig = notation[notation.length-3].match(/[a-g1-8]/)?.[0] || notation[notation.length-4].match(/[a-g1-8]/)?.[0]
 
-        
-            // convert move char to int
-            const [baseRow, baseCol] = [this.move[0], this.move.charCodeAt(0)-96]
             const colToChar = (col) => String.fromCharCode(col + 96)
 
-            let possibleCoords = []
-
-            switch(movingPeice) {
-                case 'B':
-                    // search diagonals from move coords
-                    for (let i = 1; i <= 8; i++) {
-                        possibleCoords.push([[baseRow+i, baseCol+i], [baseRow-i, baseCol-i]])
-                    }
-                    break
-                case 'K':
-                    possibleCoords = [
-                        [baseRow+1, baseCol-1], [baseRow+1, baseCol], [baseRow+1, baseCol+1],
-                        [baseRow, baseCol-1],                 [baseRow, baseCol+1],
-                        [baseRow-1, baseCol-1], [baseRow-1, baseCol], [baseRow-1, baseCol+1]
-                    ]
-                    break
-                case 'N':
-                    // search 1 baseRow + 2 cols and 2 rows + 1 baseCol from move coords
-                    possibleCoords = [
-                       [baseRow+2, baseCol-1], [baseRow+2, baseCol+1], 
-                       [baseRow+1, baseCol-2], [baseRow+1, baseCol+2],  
-                       [baseRow-1, baseCol-2], [baseRow-1, baseCol+2], 
-                       [baseRow-2, baseCol-1], [baseRow-2, baseCol+1]
-                    ]  
-                    break
-                case 'P':
-                    const orientatedRowDiff = baseRow+(toPlay === 'light' ? 1 : -1)
-                    if (takenPeice) {
-                        // search +1 and -1 baseCol + 1 baseRow toPlay === 'light' ? foward : backward
-                        possibleCoords = [[orientatedRowDiff, baseCol-1], [orientatedRowDiff, baseCol+1]]
-                    }
-                    else {
-                        // search 1 baseRow toPlay === 'light' ? foward : backward
-                        possibleCoords = [[baseRow+(toPlay === 'light' ? 1 : -1), baseCol]]
-                    }
-
-                    // if first move, include 2 baseRow move
-                    var firstMoveConditionalCoord = [baseRow+(toPlay === 'light' ? 2 : -2), baseCol]
-                    if (this.rows[firstMoveConditionalCoord[0]]?.[colToChar(firstMoveConditionalCoord[1])]?.unit?.moves === 0) {
-                        possibleCoords.push(firstMoveConditionalCoord)
-                    }
-
-                    // if opposing pawn is one baseRow above and on it's first move, include En Passant
-
-                    break
-                case 'Q':
-                    for (let i = 1; i <= 8; i++) {
-                        possibleCoords.push([[baseRow+i, baseCol+i], [baseRow-i, baseCol-i]])
-                        possibleCoords.push([
-                                [baseRow+i, baseCol], 
-                            [baseRow, baseCol-i], [baseRow, baseCol+i]
-                                [baseRow-i, baseCol], 
-                        ])
-                    }
-                    break
-                case 'R':
-                    // within baseRow and baseCol of move coords
-                    for (let i = 1; i <= 8; i++) {
-                        possibleCoords.push([
-                                [baseRow+i, baseCol], 
-                            [baseRow, baseCol-i], [baseRow, baseCol+i]
-                                [baseRow-i, baseCol], 
-                        ])
-                    }
-                    break
-            }
-
-            // check each possibleCoord
-            const findPeices = (piece, side, possibleCoords) => {
+            const findPiece = (piece, side, baseRow, baseCol) => {
+                let possibleCoords = []
                 
+                if (!baseRow && baseCol) {
+                    // all coords
+                    for (let r = 1; r <= 8; r++) {
+                        for (let c = 1; c <= 8; c++) {
+                            possibleCoords.push([[baseRow+r, baseCol+c]])
+                        }
+                    }
+                }
+                else {
+                    switch(unitToMove) {
+                        case 'B':
+                            // search diagonals from move coords
+                            for (let i = 1; i <= 8; i++) {
+                                possibleCoords.push([[baseRow+i, baseCol+i], [baseRow-i, baseCol-i]])
+                            }
+                            break
+                        case 'K':
+                            possibleCoords = [
+                                [baseRow+1, baseCol-1], [baseRow+1, baseCol], [baseRow+1, baseCol+1],
+                                [baseRow, baseCol-1],                 [baseRow, baseCol+1],
+                                [baseRow-1, baseCol-1], [baseRow-1, baseCol], [baseRow-1, baseCol+1]
+                            ]
+                            break
+                        case 'N':
+                            // search 1 baseRow + 2 cols and 2 rows + 1 baseCol from move coords
+                            possibleCoords = [
+                                [baseRow+2, baseCol-1], [baseRow+2, baseCol+1], 
+                                [baseRow+1, baseCol-2], [baseRow+1, baseCol+2],  
+                                [baseRow-1, baseCol-2], [baseRow-1, baseCol+2], 
+                                [baseRow-2, baseCol-1], [baseRow-2, baseCol+1]
+                            ]  
+                            break
+                        case 'P':
+                            const orientatedRowDiff = baseRow+(toPlay === 'light' ? 1 : -1)
+                            if (unitToTake) {
+                                // search +1 and -1 baseCol + 1 baseRow toPlay === 'light' ? foward : backward
+                                possibleCoords = [[orientatedRowDiff, baseCol-1], [orientatedRowDiff, baseCol+1]]
+                            }
+                            else {
+                                // search 1 baseRow toPlay === 'light' ? foward : backward
+                                possibleCoords = [[baseRow+(toPlay === 'light' ? 1 : -1), baseCol]]
+                            }
+
+                            // if first move, include 2 baseRow move
+                            var firstMoveConditionalCoord = [baseRow+(toPlay === 'light' ? 2 : -2), baseCol]
+                            if (this.rows[firstMoveConditionalCoord[0]]?.[colToChar(firstMoveConditionalCoord[1])]?.unit?.moves === 0) {
+                                possibleCoords.push(firstMoveConditionalCoord)
+                            }
+
+                            // if opposing pawn is one baseRow above and on it's first move, include En Passant
+
+                            break
+                        case 'Q':
+                            for (let i = 1; i <= 8; i++) {
+                                possibleCoords.push([[baseRow+i, baseCol+i], [baseRow-i, baseCol-i]])
+                                possibleCoords.push([
+                                        [baseRow+i, baseCol], 
+                                    [baseRow, baseCol-i], [baseRow, baseCol+i]
+                                        [baseRow-i, baseCol], 
+                                ])
+                            }
+                            break
+                        case 'R':
+                            // within baseRow and baseCol of move coords
+                            for (let i = 1; i <= 8; i++) {
+                                possibleCoords.push([
+                                        [baseRow+i, baseCol], 
+                                    [baseRow, baseCol-i], [baseRow, baseCol+i]
+                                        [baseRow-i, baseCol], 
+                                ])
+                            }
+                            break
+                    }
+                }
+
+                if (specifiedOrig) {
+                    possibleCoords = possibleCoords.filter(([row, col]) => Number.isInteger(specifiedOrig) ? row === specifiedOrig : colToChar(col) === specifiedOrig)
+                }
+
+                // check each possibleCoord
+                let cells = []
                 for (let [row, col] of possibleCoords) {
                     const cell = this.rows[row][col]
-                    if (cell.side != side) {
+                    if (cell.side != side || cell.unit != piece) {
                         continue
                     }
-
+                    cells.push({data: cell, location: row+colToChar(col)})
                 }
+                if (cells.length > 1) {
+                    console.warn(`Notation ambigous, multiple possible pieces!: ${JSON.stringify(cells)}`)
+                }
+                else if (cells.length === 0) {
+                    console.warn(`No valid cells found for notation!: ${notation}`)
+                }
+
+                return cells[0]
             }
-            const cells = findPeices(movingPeice, toPlay, possibleCoords)
 
+            // if move not in notation find unitToTake
+            let pieceToTake;
+            if (!move) {
+                if (!unitToTake) {
+                    console.warn(`Invalid notation!: ${notation}`)
+                    return 
+                }
 
+                const cell = findPiece(unitToTake, toPlay === 'light' ? 'dark' : 'light')
+                pieceToTake = cell?.data
+                move = cell?.location
+            }
 
+            // convert move char to int
+            const [baseRow, baseCol] = [move[0], move.charCodeAt(0)-96]
+            
+            const cell = findPiece(unitToMove, toPlay, baseRow, baseCol)
+            const pieceToMove = cell.data
+            pieceToMove.unit.moves++
+            this.rows[move[0]][move[1]].unit = { ...pieceToMove }
+            this.rows[cell.location[0]][cell.location[1]].unit = {}
+
+            return {
+                from: { row: cell.location[0], col: cell.location[1] },
+                pieceMoved: pieceToMove,
+                pieceTaken: pieceToTake,
+                on: { row: move[0], col: move[1] }
+            }
+            
         }
     }
 }
